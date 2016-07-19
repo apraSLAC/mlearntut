24880595e48df16100b48562c8164b92015afb36
Abdullah P Rashed Ahmed <apra@slac.stanford.edu>
2016-07-19 14:30:37 -0700

    Added an SVM training routine to see how it performs

--- regression.py
+++ regression.py
@@ -11,10 +11,16 @@ import math
 import numpy as np
 import h5py
 from collections import namedtuple
-
-import tensorflow as tf
+try:
+	import tensorflow as tf
+	import TFModel
+except:
+    print("Could not import tensorflow")
+try:
+	from sklearn import svm
+except:
+	print("Could not import sklearn")
 import MLUtil as util
-import TFModel
 
 def getTrainData(mode='test'):
     t0 = time.time()
@@ -28,7 +34,7 @@ def getTrainData(mode='test'):
                 validation_X=validation_X, 
                 validation_Y=validation_Y)
     
-def train(saved_model, trainData=None):
+def trainNN(saved_model, trainData=None):
     if trainData is None:
         trainData = getTrainData('test')
     numOutputs, training_X, training_Y, validation_X, validation_Y = \
@@ -106,7 +112,59 @@ def train(saved_model, trainData=None):
     save_path = saver.save(sess, saved_model + '_final')
     print(' ** saved final model in %s' % save_path)
             
+def trainSVM(trainData=None):
+    if trainData is None:
+        trainData = getTrainData('test')
+    numOutputs, training_X, training_Y, validation_X, validation_Y = \
+        trainData.numOutputs, trainData.training_X, trainData.training_Y, \
+        trainData.validation_X, trainData.validation_Y
 
+    minibatch_size = 64  
+    batches_per_epoch = len(training_X)//minibatch_size
+    print("batch size=%d gives %d batches per epoch" % (minibatch_size, batches_per_epoch))
+    sys.stdout.flush()
+
+    VALIDATION_SIZE = 128
+    util.shuffle_data(validation_X, validation_Y)
+    validation_X = validation_X[0:VALIDATION_SIZE]
+    validation_Y = validation_Y[0:VALIDATION_SIZE]
+
+    svr = svm.SVR()
+    nusvr = svm.nuSVR()
+    lsvr = svm.LinearSVR()
+
+    print("ep bch tm1 trLoss1 vlLoss1 tm2 trLoss2 vlLoss2 tm3 trLoss3 vlLoss3")
+    sys.stdout.flush()
+    for epoch in range(4):
+        util.shuffle_data(training_X, training_Y)
+        next_sample_idx = -minibatch_size
+        for batch in range(batches_per_epoch):
+            next_sample_idx += minibatch_size
+            X=training_X[next_sample_idx:(next_sample_idx+minibatch_size),:]
+            Y=training_Y[next_sample_idx:(next_sample_idx+minibatch_size),:]
+            t0 = time.time()
+            svr = svr.fit(X,Y)
+            tm1 = time.time() - t0
+            trLoss1 = svr.score(X,Y)
+            vlLoss1 = svr.score(validation_X, validation_Y)
+
+            t1 = time.time()
+            nusvr = nusvr.fit(X,Y)
+            tm2 = time.time() - t1
+            trLoss2 = nusvr.score(X,Y)
+            vlLoss2 = nusvr.score(validation_X, validation_Y)
+
+            t2 = time.time()
+            lsvr = lsvr.fit(X,Y)
+            tm3 = time.time() - t2
+            trLoss3 = lsvr.score(X,Y)
+            vlLoss3 = lsvr.score(validation_X, validation_Y)
+            
+            print("{0:1} {1:>5} {2:^15} {3:^23} {4:^31} {5} {6} {7} {8} {9} {10}".format(
+                epoch, batch, tm1, trLoss1, vlLoss1, tm2, trLoss2, vlLoss2, tm3, 
+                trLoss3, vlLoss3))
+            sys.stdout.flush()
+    
 def with_graph(saved_model, cmd):
     if cmd == 'train':
         train(saved_model)
@@ -123,7 +181,10 @@ if __name__ == '__main__':
     print("-- imports done, starting main --")
     cmd = sys.argv[1].lower().strip()
     saved_model = 'tf_saved_2color_model'
-
-    with tf.Graph().as_default():
-        with_graph(saved_model, cmd)
+    
+    if cmd == 'svm':
+        trainSVM()
+    else:
+	    with tf.Graph().as_default():
+	        with_graph(saved_model, cmd)
 
